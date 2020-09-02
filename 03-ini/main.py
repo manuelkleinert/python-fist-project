05a103a6d755f623517c7afb21b5a35deb273b3f
Manuel Kleinert <info@manuelkleinert.ch>
2020-09-02 16:51:17 +0200

    Problem with read ini file

--- 03-ini/main.py
+++ 03-ini/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+import os
 from configparser import ConfigParser
 
+root = os.path.abspath(os.curdir)
+
 config = ConfigParser()
-config.read('config.ini')
+config.read(['config.ini'])
 
 print(config)
+print(os.path.abspath('config.ini'));
+print(os.path.isfile(os.path.abspath('config.ini')))
 print ("Sections : ", config.sections())
 print ("Sections : ", config.has_section('settings'))
 
