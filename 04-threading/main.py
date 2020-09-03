4f04eb8bd07c3d2efba51aabc8ebefedbfe806c1
Manuel Kleinert <info@manuelkleinert.ch>
2020-09-03 10:18:50 +0200

    add thread test

---
+++ 04-threading/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+import threading
+import time
+
+def thread_function(name, sleep):
+    logging.info("Thread %s: starting", name)
+    time.sleep(sleep)
+    logging.info("Thread %s: finishing", name)
+
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+
+    x1 = threading.Thread(target=thread_function, args=(1,4))
+    x2 = threading.Thread(target=thread_function, args=(2,3))
+
+    logging.info("Main    : before running thread")
+
+    x1.start()
+    x2.start()
+
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
