e1ddec7f36a81fdaa54ca9b457b585161cb7de10
manuelkleinert <manuel.kleinert@gmail.com>
2020-09-23 12:13:25 +0200

    update

--- 06-arduino-connect/main.py
+++ 06-arduino-connect/main.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-
 from tkinter import *
-from classes.taps import Taps
 
 app = Tk()
 
@@ -11,14 +9,3 @@ app.geometry('600x600')
 app.resizable(width=False, height=False)
 
 taps = Taps(app)
-
-frame1 = taps.addFrame('test 1')
-frame2 = taps.addFrame('test 2')
-
-content = Label(frame1, text = 'test 1 content')
-content.grid(column = 0, row = 0, padx = 30, pady = 30)
-
-content2 = Label(frame2, text = 'test 2 content')
-content2.grid(column = 0, row = 0, padx = 30, pady = 30)
-
-app.mainloop()
